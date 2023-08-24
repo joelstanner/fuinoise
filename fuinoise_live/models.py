e6701aefa9c0f8127dcd6a02d6d9c2b12bf598e3
joelstanner <joel@joelstanner.com>
2023-08-24 02:16:52 -0700

    initial models

--- fuinoise_live/models.py
+++ fuinoise_live/models.py
@@ -1,18 +1,33 @@
-import datetime
-
 from django.contrib import admin
 from django.db import models
-from django.utils import timezone
-
-
-class Event(models.Model):
-    start = models.DateTimeField()
-    end = models.DateTimeField()
-    name = models.CharField(max_length=255)
 
 
 class Streamer(models.Model):
-    name = models.CharField(max_length=255)
+    display_name = models.CharField(max_length=80)
+    twitch_username = models.CharField(max_length=80)
     twitch_url = models.URLField()
-    homepage = models.URLField()
-    twitch_id = models.IntegerField()
+    homepage = models.URLField(blank=True)
+    twitch_id = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.display_name
+
+class Event(models.Model):
+    date = models.DateField("Calendar Start Date of the event")
+    name = models.CharField(default="Raid Train", max_length=255)
+    streamers = models.ManyToManyField(Streamer, through="RaidSlot")
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class RaidSlot(models.Model):
+    streamer = models.ForeignKey(Streamer, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    replay_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return print(f"{self.streamer}-{self.event}")
